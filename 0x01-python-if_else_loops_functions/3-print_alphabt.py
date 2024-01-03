8b49f08ce407466a7cc0263a005228a2eb3e9f86
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-01-03 02:51:26 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+x = 97
+ch_string = ""
+while x <= 122:
+    if ch(x) != 'e' and ch(x) != 'q':
+        output_string += chr(x)
+        x += 1
+print("{}".format(output_string), end="")
