2a4ccc5c3dd0862e0c1d6d80afa199a3670ad746
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-01-04 07:18:36 -0800

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
 a = 1
 b = 2
-print("1 + 2 = {}".format(add(a, b)))
+from add_0 import add
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
