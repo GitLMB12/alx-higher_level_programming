56a7de75a2a7633302d7136d6de162ed478fc737
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-01-04 16:06:58 -0800

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def print_arguments():
         print("0 arguments.")
     else:
         print("{} argument{}:".format(num_arguments, "s" if num_arguments > 1 else ""))
-    for i, arg in enumerate(argv[1:], start=1):
+    for i, arg in enumerate(argv[1:], start = 1):
         print("{}: {}".format(i, arg))
 if __name__ == "__main__":
     print_arguments()
