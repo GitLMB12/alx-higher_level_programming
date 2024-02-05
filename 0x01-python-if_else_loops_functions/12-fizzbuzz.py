86fa84a5c0dc3373bfb08e51054d523ff1310cc7
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-02-05 07:21:26 -0800

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
- def fizzbuzz():
-     for Num in range (1, 101):
-         if Num % 3 == 0:
-             print(f"Fizz", end" ")
-         elif Num % 5 == 0:
-             print(f"Buzz", end" ")
-         else:
-             print(f"{Num}", end" ")
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(number, end=" ")
