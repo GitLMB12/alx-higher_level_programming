9d5ef9ec5d2e5ca44e7727778a3d191a00d813b6
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-02-07 13:02:37 -0800

    r

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,8 @@ def infinite_add(arguments):
     total = 0
     for arg in arguments:
         total += int(arg)
-    print(total)
+    return total
 if __name__ == "__main__":
     arguments = argv[1:]
-    infinite_add(arguments)
+    result = infinite_add(arguments)
+    print(result )
