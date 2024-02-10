ce6d2f7b6c47a0d0c7cc608e48a69939e3e0c157
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-02-10 07:36:32 -0800

    r

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    nb_print = 0
+    nb_printed = 0
     try:
         for i in range x:
             print(my_list[i], end="")
-            nb_print += 1
+            nb_printed += 1
     except IndexError:
         pass
     print()
-    return nb_print
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    safe_print_list(my_list)
+    return nb_printed
