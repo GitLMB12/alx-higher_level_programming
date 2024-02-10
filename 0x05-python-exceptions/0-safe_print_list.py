e8c3b3babcf5d598471458281076d8790844743a
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-02-10 07:22:48 -0800

    r

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range x:
             print("{}".format(my_list[i]), end="")
             nb_print += 1
-    except:
+    except IndexError:
         print(end="")
     print()
     return (nb_print)
